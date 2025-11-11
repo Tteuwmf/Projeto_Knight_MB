2b20e9d2900870dc8e87c8cd4df94638f05e517f
Tteuwmf <faccini0910@gmail.com>
2025-11-10 22:43:41 -0300

    começando a por o game

---
+++ game.c
@@ -0,0 +1,63 @@
+#include "raylib.h"
+#include "game.h"
+/*
+Game createGame()
+{
+
+}
+
+
+void initGame()
+{
+
+
+
+
+
+
+    switch()
+            {
+
+                case 0:
+
+                    if(IsKeyPressed(KEY_ENTER))
+                    {
+                        lobbyOrWorld++;
+
+                        if(gameWindow->gameWorldInitiate)
+                        {
+                            gameWindow->gw->player->pos = playerGameWorldfirstPos;
+                        }
+                    }
+
+                    inputAndUpdateGameLobby(&gameWindow->gl, isFullScreen);
+
+                    drawGameLobby(&gameWindow->gl);
+                    break;
+
+                case 1:
+
+                    if(IsKeyPressed(KEY_ENTER))
+                    {
+                        lobbyOrWorld--;
+                        gameWindow->gl.player.pos = playerLobbyfirstPos;
+                    }
+
+                    if(gameWindow->gameWorldInitiate==false)
+                    {
+                        gameWindow->gw = createGameWorld(&gameWindow->gl.player);
+
+                        gameWindow->gameWorldInitiate = true;
+                    }
+
+                    if (playerGameWorldfirstPos.x==0 && playerGameWorldfirstPos.y ==0)
+                        {
+                            playerGameWorldfirstPos = gameWindow->gw->player->pos;
+                        }
+
+                    inputAndUpdateGameWorld(gameWindow->gw, isFullScreen);
+
+                    drawGameWorld(gameWindow->gw);
+                    break;
+            }
+}*/
