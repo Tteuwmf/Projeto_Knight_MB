75fb3c928c2a59fd4db2a62ae2dd651122e6b30d
Bruno Gundlach Marquesan <bruno16mar@gmail.com>
2025-12-04 22:29:54 -0300

    fix(gameplay): resolve input conflict and collision issue

--- game.c
+++ game.c
@@ -54,6 +54,7 @@ void initGame(Game *game, bool isFullScreen)
             break;
 
         case HELP:
+            inputUpdateAndDrawHelp(game, isFullScreen);
             break;
 
         case PAUSE:
